4e3a5fec807ba17e836ae682cd9c6033b268e1f5
Mustafa Keser <keserzade@gmail.com>
2023-07-12 23:29:57 +0300

    12.07.2023

--- src/pages/common/about/page.jsx
+++ src/pages/common/about/page.jsx
@@ -1,5 +1,24 @@
+import {
+    BestOffers,
+    PageHeader,
+    Spacer,
+    Team,
+    WhatWeDo,
+    WhoWeAre,
+} from "../../../components";
 const AboutPage = () => {
-    return <div>AboutPage</div>;
+    return (
+        <>
+            <PageHeader title="About Us" />
+            <Spacer />
+            <WhoWeAre />
+            <Spacer />
+            <BestOffers />
+            <Spacer />
+            <Team />
+            <Spacer />
+            <WhatWeDo />
+        </>
+    );
 };
-
 export default AboutPage;
