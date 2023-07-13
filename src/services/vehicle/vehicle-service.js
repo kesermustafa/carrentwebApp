b4ebc1fe9dac9507cb413d27f56ed63bab07e197
Mustafa Keser <keserzade@gmail.com>
2023-07-13 23:18:38 +0300

    13.07.2023

--- src/services/vehicle/vehicle-service.js
+++ src/services/vehicle/vehicle-service.js
@@ -1,9 +1,17 @@
+import axios from "axios";
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
 
 // COMMON ENDPOINTS
 export const getVehicleById = () => { };
-export const getVehicles = () => { };
-export const getVehiclesByPage = () => { };
+export const getVehicles = async () => {
+
+    const response = await axios.get(`${API_URL}/car/visitors/all`);
+    return response.data; // handle empty array case for error handling in component side
+ };
+
+
+export const getVehiclesByPage = async () => { };
 
 // ADMIN ENDPOINTS
 export const addVehicle = () => { };
