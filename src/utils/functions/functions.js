a016822f0befff66760e409b997101aa7e8d2554
Mustafa Keser <keserzade@gmail.com>
2023-07-20 23:39:05 +0300

    20.07

--- src/utils/functions/functions.js
+++ src/utils/functions/functions.js
@@ -1,5 +1,14 @@
 import Swal from 'sweetalert2';
 
+// FORM CHECK FUNCTIONS
+export const validCheck = (field, obj) => {
+    const myObject = {
+        isValid: obj.touched[field] && !obj.errors[field],
+        isInvalid: obj.touched[field] && obj.errors[field]
+    }
+    return myObject;
+};
+
 // SWEET ALERT FUNCTIONS
 export const swalQuestion = (title, text) => {
     return Swal.fire({
